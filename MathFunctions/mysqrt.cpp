7d597e4be9ca7ec7f3dfc86ffa65edf193228f2d
Ron <ronll64@gmail.com>
2014-12-09 00:15:34 +0200

    * Added log and exp functions existance checking, and using them in case they do exist. * Removed install targets * Added MakeTable.cpp

--- MathFunctions/mysqrt.cpp
+++ MathFunctions/mysqrt.cpp
@@ -1,6 +1,12 @@
 #include "MathFunctions.h"
+#include "../Config.h"
 
 double mysqrt(double value)
 {
-	return 1.0;
+#if defined (HAVE_LOG) && defined (HAVE_EXP)
+    return exp(log (value) * 0.5);
+#else
+    (void)(value);
+    return 1.0;
+#endif //defined (HAVE_LOG) && defined (HAVE_EXP)
 }
